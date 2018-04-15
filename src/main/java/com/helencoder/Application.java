02f0c271f6bcbbd6af182280859c17e8240d8b5c
helencoder <731873664@qq.com>
2018-04-15 19:32:23 +0800

    重构

---
+++ src/main/java/com/helencoder/Application.java
@@ -0,0 +1,25 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+/**
+ * Spring 启动脚本
+ *
+ * Created by helencoder on 2018/2/6.
+ */
+//@ComponentScan(basePackages = "com.helencoder")
+//@EnableAutoConfiguration
+//public class Application {
+//    public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+//    }
+//}
+
+
+@EnableCaching
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
